92ea6753fd481d6f5ec512a8092ae2e085791b9e
Joe Melia <jmelia@students.nortwheststate.edu>
2025-06-10 18:12:34 -0400

    Assignment Complete

---
+++ Chapter 1 Lab 1.py
@@ -0,0 +1,7 @@
+print("Deep into that darkness peering, long I stood there wondering, fearing,\n",
+"Doubting, dreaming dreams no mortals ever dared to dream before;\n",
+"But the silence was unbroken, and the stillness gave no token,\n",
+"And the only word there spoken was the whispered word, 'Lenore?'\n",
+"This I whispered, and an echo murmured back the word, 'Lenore!'\n",
+"Merely this, and nothing more.\n",
+"Joe Melia")
